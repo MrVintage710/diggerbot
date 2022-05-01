f8c84e7acc75cf17a1f98e0bf3f6138466bf39b4
Brooks Palin <brookspalin710@gmail.com>
2022-05-01 13:25:33 -0600

    slow mode

--- controller.py
+++ controller.py
@@ -16,8 +16,9 @@ pwm_3 = gpio.PWM(33, 1000)
 pwm_4 = gpio.PWM(35, 1000)
 
 max_duty_cycle = 65
+slow_mode_duty_cycle = 50
 
-max_back_duty_cycle = 40
+slow_mode = false
 
 gamepadType = Gamepad.XboxONE
 
@@ -43,23 +44,28 @@ try:
 			pwm_4.ChangeDutyCycle(max_duty_cycle)
 		else:
 			direction = gamepad.isPressed("A");
-			slow_mode = gamepad.isPressed("")
+			if gamepad.beenPressed("X"):
+				slow_mode = !slow_mode
 			if gamepad.beenPressed("Y"):
 				break
 			#if gamepad.beenPressed("A"):
 			#	print("A")
 			right_speed = (gamepad.axis("RT") + 1.0) / 2.0
 			left_speed = (gamepad.axis("LT") + 1.0) / 2.0
+
+			right_final = max_duty_cycle * right_speed if not slow_mode else max_back_duty_cycle * right_speed
+			left_final = max_duty_cycle * left_speed if not slow_mode else max_back_duty_cycle * left_speed
+
 			if direction:
-				pwm_1.ChangeDutyCycle(left_speed * max_back_duty_cycle)
+				pwm_1.ChangeDutyCycle(left_final)
 				pwm_2.ChangeDutyCycle(0)
-				pwm_3.ChangeDutyCycle(right_speed * max_back_duty_cycle)
+				pwm_3.ChangeDutyCycle(right_final)
 				pwm_4.ChangeDutyCycle(0)
 			else:
 				pwm_1.ChangeDutyCycle(0)
-				pwm_2.ChangeDutyCycle(left_speed * max_duty_cycle)
+				pwm_2.ChangeDutyCycle(left_final)
 				pwm_3.ChangeDutyCycle(0)
-				pwm_4.ChangeDutyCycle(right_speed * max_duty_cycle)
+				pwm_4.ChangeDutyCycle(right_final)
 		
 finally:
 	gpio.cleanup()
